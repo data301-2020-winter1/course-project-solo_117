64605f01dffb86e2924c91b4412293dd332bca51
JackPrice98 <jack-price11@outlook.com>
2020-11-14 19:57:24 -0800

    Jack Price, Task 4

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+def load_and_process(filePathCSV):
+    df1 = (pd.read_csv(filePathCSV)
+           .drop(['TOI/GP','FOW%','S%'], axis=1)
+           .rename(columns = {'GP':'GamesPlayed' , '+/-':'PlusMinus', 'PIM':'PenaltyMins', 'S':'Shots'})
+    )
+    df2 = (df1 
+           .loc[lambda x: x['Shots']>50]
+           .loc[lambda x: x['GamesPlayed']>70]
+           .sort_values(by = ['G'], ascending = False)
+    )
+    return df2
+   
+    
