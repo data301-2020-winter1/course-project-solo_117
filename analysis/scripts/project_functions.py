f74c14d00ca65f5a370e698a925ea976b7077fb3
JackPrice98 <jack-price11@outlook.com>
2020-11-25 20:57:29 -0800

    JackPrice milestone3

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -4,7 +4,7 @@ import numpy as np
 def load_and_process(filePathCSV):
     df1 = (pd.read_csv(filePathCSV)
            .drop(['TOI/GP','FOW%','S%'], axis=1)
-           .rename(columns = {'GP':'GamesPlayed' , '+/-':'PlusMinus', 'PIM':'PenaltyMins', 'S':'Shots'})
+           .rename(columns = {'GP':'GamesPlayed' , '+/-':'PlusMinus', 'PIM':'PenaltyMins', 'S':'Shots', 'S/C':'Hand'})
     )
     df2 = (df1 
            .loc[lambda x: x['Shots']>50]
